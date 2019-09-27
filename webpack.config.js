e949af2cf03a2fdff7c03866c8ea1759ddc5ccd9
CharlesSigo <chuck@schooldatebooks.com>
2019-09-27 15:34:24 -0400

    add copy-webpack-plugin and clean-webpack-plugin to refresh the dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,30 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 module.exports = {
-  entry: './src/app.ts',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      }
-    ]
-  },
-  resolve: {
-    extensions: [ '.ts', '.tsx', '.js' ]
-  },
-  output: {
-    filename: 'app.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  mode: 'development'
+    entry: './src/app.ts',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new CopyWebpackPlugin([
+            { from: 'src/assets', to: 'assets' }
+        ]),
+
+    ],
+    resolve: {
+        extensions: [ '.ts', '.tsx', '.js' ]
+    },
+    output: {
+        filename: 'app.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    mode: 'development'
 };
